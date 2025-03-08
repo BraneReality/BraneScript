554106d3822bf981761ef2b241c0534b4dbccda5
WireWhiz <eli@knisley.info>
2025-03-08 03:05:52 -0500

    convert parser to rust

--- .vscode/triplets/zig-x64-windows.cmake
+++
@@ -1,6 +0,0 @@
-set(VCPKG_TARGET_ARCHITECTURE x64)
-set(VCPKG_CRT_LINKAGE static) # Or dynamic if you prefer
-set(VCPKG_LIBRARY_LINKAGE static)
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../toolchains/toolchain-zig-windows.cmake")
-set(VCPKG_CMAKE_SYSTEM_NAME Windows)
-set(VCPKG_ENV_PASSTHROUGH PATH)
---
+++ brane_script_compiler/src/parsed_document.rs
@@ -0,0 +1,70 @@
+use crate::{
+    document_context::{DocumentContext, TextSource},
+    parser::{nodes::SourceFile, DocumentParser},
+};
+use anyhow::anyhow;
+use std::{path::PathBuf, sync::Arc};
+use type_sitter::{Node, Parser, Tree};
+
+pub use crate::parser::ParserMessage;
+
+pub struct ParsedDocument {
+    parser: Parser<SourceFile<'static>>,
+    tree: Option<Tree<SourceFile<'static>>>,
+    path: PathBuf,
+    source: String,
+    cached_ctx: Option<DocumentContext>,
+    messages: Vec<ParserMessage>,
+}
+
+impl ParsedDocument {
+    pub fn new(path: PathBuf, current_source: Option<String>) -> anyhow::Result<ParsedDocument> {
+        let current_source = if let Some(source_text) = current_source {
+            source_text
+        } else {
+            std::fs::read_to_string(&path)?
+        };
+
+        let parser = Parser::new(&tree_sitter_branescript::LANGUAGE.into())?;
+        Ok(ParsedDocument {
+            parser,
+            tree: None,
+            path,
+            source: current_source,
+            cached_ctx: None,
+            messages: Vec::new(),
+        })
+    }
+
+    pub fn source(&self) -> &str {
+        return self.source.as_str();
+    }
+
+    pub fn get_document_context(&mut self) -> anyhow::Result<&DocumentContext> {
+        let source = Arc::new(TextSource {
+            uri: format!("file://{}", self.path.to_string_lossy()),
+        });
+
+        self.tree = Some(
+            self.parser
+                .parse(&self.source, self.tree.as_ref())
+                .map_err(|_: ()| anyhow!("Failed to parse document"))?,
+        );
+        let tree = self.tree.clone().unwrap();
+
+        let source_file = tree.root_node().map_err(|err| {
+            anyhow!(format!(
+                "Was expecting {} but found \"{}\" with content: {}",
+                err.kind,
+                err.node.kind(),
+                &self.source[err.node.byte_range()]
+            ))
+        })?;
+        let parseResult = DocumentParser::parse_document(&self.source, source_file, source);
+        if parseResult.document.is_some() {
+            self.cached_ctx = parseResult.document;
+        }
+
+        return Ok(self.cached_ctx.as_ref().unwrap());
+    }
+}
