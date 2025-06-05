b02dc8fdf498a0ce36af292d39356c44b1f4b07a
WireWhiz <eli@knisley.info>
2025-06-05 04:55:45 -0400

    template out query based hir

--- compiler/branec/src/lib.rs
+++ compiler/branec/src/lib.rs
@@ -1,1 +1,29 @@
-//pub mod compiler;
+use std::{
+    collections::HashMap,
+    path::PathBuf,
+    sync::{Arc, RwLock},
+};
+
+pub use branec_hir::hir::{self, Graph, GraphId};
+
+#[salsa::db]
+#[derive(Clone, Default)]
+pub struct ProjectCtx {
+    storage: salsa::Storage<Self>,
+    //pub workspace: Arc<PathBuf>,
+    //pub path_ids: Arc<RwLock<HashMap<PathBuf, GraphId>>>,
+    //pub loaded_graphs: Arc<RwLock<HashMap<GraphId, LoadedGraphCtx>>>,
+}
+
+#[salsa::db]
+impl salsa::Database for ProjectCtx {}
+
+pub enum GraphLoadMode {
+    Mutable,
+    Immutable,
+}
+
+pub struct LoadedGraphCtx {
+    pub graph: RwLock<Graph>,
+    pub load_mode: GraphLoadMode,
+}
